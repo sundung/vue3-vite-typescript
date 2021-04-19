9ff57affdeb4ec2ccab3bea2206ca8c6b6927e90
sunzhiqiang <sun1358742@163.com>
2021-04-19 17:39:44 +0800

    使用vuex管理项目全局状态

--- src/main.ts
+++ src/main.ts
@@ -4,4 +4,7 @@ import App from './App.vue'
 // 引入路由
 import router from './router/index'
 
-createApp(App).use(router).mount('#app')
+// 引入 vuex
+import store from './store/index'
+
+createApp(App).use(router).use(store).mount('#app')
