9f822820a5a794d9f4d3ebbe4d075b3a5ae79018
sunzhiqiang <sun1358742@163.com>
2021-04-20 09:39:56 +0800

    集成 UI 框架 Element Plus

--- src/main.ts
+++ src/main.ts
@@ -7,4 +7,13 @@ import router from './router/index'
 // 引入 vuex
 import store from './store/index'
 
-createApp(App).use(router).use(store).mount('#app')
+// 集成 UI 框架 Element Plus
+
+import ElementPlus from 'element-plus'
+import 'element-plus/lib/theme-chalk/index.css'
+
+const app = createApp(App)
+app.use(ElementPlus)
+app.use(router)
+app.use(store)
+app.mount('#app')
