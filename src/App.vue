b7e7ba64fba90c6275c47a54c5f9bb456eb3387d
sunzhiqiang <sun1358742@163.com>
2021-04-20 10:01:12 +0800

    下载axios,以及使用css预处理器stylus

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,27 @@
 <template>
-  <el-button type="primary">主要按钮qq</el-button>
+  <div>
+    <el-button type="primary">主要按钮qq</el-button>
+    <testAxios></testAxios>
+    <div class="test">测试stylus</div>
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
-
+import testAxios from "./components/testAxios.vue";
 export default defineComponent({
-  name: "App"
+  name: "App",
+  components: {
+    testAxios
+  }
 });
 </script>
 
-<style>
+<style lang='stylus' scoped>
+.test {
+  color: red;
+  background-color: pink;
+  width: 110px;
+  height: 40px;
+}
 </style>
